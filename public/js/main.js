ad685cb16a9018268048a1158a4cba6d675822fc
Toxic17 <49877504+Toxic17@users.noreply.github.com>
2024-06-30 23:42:04 +0300

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,12 @@
 let btns = document.querySelectorAll('.answer');
+let form = document.querySelector('.main-form');
+
 
 for(let i =0;i<btns.length;i++)
 {
-    btns[i].addEventListener('click',function(){
-
+    btns[i].addEventListener('click',function(e){
+        e.preventDefault();
+        this.insertAdjacentHTML("afterend",form.outerHTML);
+        form.innerHTML = '';
     })
 }
